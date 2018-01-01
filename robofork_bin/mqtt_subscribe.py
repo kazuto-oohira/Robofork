35d0836183dbecdb70d2426df3fe57b201ae5645
Shingo Mori <shingo-mori@azapa.co.jp>
2018-01-02 01:53:09 +0900

    MQTTテストを改善

--- robofork_bin/mqtt_subscribe.py
+++ robofork_bin/mqtt_subscribe.py
@@ -13,7 +13,7 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    # print(msg.topic + " " + str(msg.payload))
     requests.post('http://127.0.0.1:8000/mqtt_test/receive', data={'payload': msg.payload})
 
 
