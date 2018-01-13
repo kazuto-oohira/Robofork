b492a7c60817d08173e49dbb0b4a623e8b3eff8d
Shingo Mori <shingo-mori@azapa.co.jp>
2018-01-14 06:20:16 +0900

    d3を導入して様子をみた

--- robofork_app/static/robofork_app/js/operation_plan_detail.js
+++ robofork_app/static/robofork_app/js/operation_plan_detail.js
@@ -2,10 +2,23 @@ if (typeof RBFK === "undefined") {
     var RBFK = {};
 }
 
-RBFK.operation = (function(global) {
+RBFK.operationPlan = (function(global) {
     var self = function() {};
 
-
+    self.init = function() {
+        var color = d3.scale.category10();
+        var svg = d3.select("#map-draw-layer");
+        svg.append('circle').attr({
+            'cx': 100,
+            'cy': 90,
+            'r': 20,
+            'fill': color(0),
+        });
+    };
 
     return self;
 })(window);
+
+$(function() {
+    RBFK.operationPlan.init();
+});
