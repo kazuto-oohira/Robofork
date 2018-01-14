062ba3dc83460f6f57ec8ee452b4b3b5b529e397
Shingo Mori <shingo-mori@azapa.co.jp>
2018-01-14 16:38:00 +0900

    MasterMarkerが置けるようになった

--- robofork_app/static/robofork_app/js/operation_plan_detail.js
+++ robofork_app/static/robofork_app/js/operation_plan_detail.js
@@ -2,23 +2,44 @@ if (typeof RBFK === "undefined") {
     var RBFK = {};
 }
 
+// テストデータ
+RBFK.operationPlanData = {
+    'master-markers-r': 8,
+    'master-markers': [
+        { x:69, y:104 },
+        { x:69, y:198 },
+        { x:69, y:198 },
+        { x:194, y:245 },
+        { x:194, y:245 },
+        { x:231, y:198 },
+        { x:231, y:198 },
+        { x:231, y:104 }
+    ]
+};
+
 RBFK.operationPlan = (function(global) {
     var self = function() {};
 
-    self.init = function() {
+    var initialize = function() {
         var color = d3.scale.category10();
-        var svg = d3.select("#map-draw-layer");
-        svg.append('circle').attr({
-            'cx': 100,
-            'cy': 90,
-            'r': 20,
-            'fill': color(0),
-        });
+        self.svg = d3.select("#map-draw-layer");
+
+        // マスターマーカーを設定
+        self.svg.selectAll("circle").data(RBFK.operationPlanData['master-markers']).enter()
+            .append('circle')
+            .attr({
+                'r': RBFK.operationPlanData['master-markers-r'],
+                'fill': 'blue',
+                'cx': function(d) { return d['x']; },
+                'cy': function(d) { return d['y']; }
+            });
     };
+
+    self.initialize = initialize;
 
     return self;
 })(window);
 
 $(function() {
-    RBFK.operationPlan.init();
+    RBFK.operationPlan.initialize();
 });
