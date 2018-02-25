cabe05a8845cd42d7b611907a6933f2f73bf4216
Yuya Yamada <y@autotelic.jp>
2018-02-26 01:26:35 +0900

    メインノードサブノードに分けインターフェース見直し

--- robofork_frontend/src/MapViewer.vue
+++ robofork_frontend/src/MapViewer.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="map-viewer">
-    <div id="map-container">
-      <img v-if="mapImage" id="map-img" :src="mapImage.url" :width="mapImage.width" :height="mapImage.height">
+    <div id="map-container" :style="containerStyles">
+      <img v-if="config" id="map-img" :src="config.imageUrl">
       <svg id="map-draw-layer">
-        <circle v-for="node in nodes" r="8" fill="blue" :cx="node.x" :cy="node.y" @click="click(node)"></circle>
+        <circle v-for="(node, index) in mainNodes" r="8" fill="blue" :cx="mapX[index]" :cy="mapY[index]" @click="click(node)" :title="node.id"></circle>
       </svg>
     </div>
   </div>
@@ -16,29 +16,61 @@ const OPERATION_ENDPOINT = '/static/robofork_app/api/operation_control.json';
 
 export default {
   name: 'map-viewer',
-  data () {
+
+  data() {
     return {
-      nodes: [],
-      mapImage: {},
+      mainNodes: [],
+      config: {},
     }
   },
 
-  methods: {
-    click: function(node) {
-      window.alert(`${node.x} / ${node.y}`);
+  computed: {
+    containerStyles: function() {
+      return {
+        width: `${this.config.imageWidth}px`,
+        height: `${this.config.imageHeight}px`,
+      };
+    },
+    unitX: function() {
+      return this.config.imageWidth / Number(this.config.scaleX);
+    },
+    unitY: function() {
+      return this.config.imageHeight / Number(this.config.scaleY);
+    },
+    mapX: function() {
+      return this.mainNodes.map((item) => {
+        const offsetX = Number(item.x) + Number(this.config.offsetX);
+        return offsetX * this.unitX + this.config.imageWidth;
+      });
+    },
+    mapY: function() {
+      return this.mainNodes.map((item) => {
+        const offsetY = Number(item.y) + Number(this.config.offsetY);
+        return -offsetY * this.unitY;
+      });
     },
   },
 
   created() {
     axios.get(OPERATION_ENDPOINT)
       .then((resp) => {
-        if ('nodes' in resp.data) {
-          this.nodes = resp.data.nodes;
+        if ('config' in resp.data) {
+          this.config = resp.data.config;
         }
-        if ('mapImage' in resp.data) {
-          this.mapImage = resp.data.mapImage;
+        if ('mainNodes' in resp.data) {
+          this.mainNodes = resp.data.mainNodes;
         }
       });
+  },
+
+  methods: {
+    click: function(node) {
+      alert(`node.id is ${node.id}`);
+    },
+
+    reset: function() {
+      this.mainCommands.reset();
+    },
   },
 }
 </script>
@@ -46,20 +78,19 @@ export default {
 <style>
 #map-container {
   position: relative;
-  width: 460px;
-  height: 580px;
+  select: none;
 }
 #map-img {
+  width: 100%;
+  height: 100%;
   position: relative;
-  width: 460px;
-  height: 580px;
 }
 #map-draw-layer {
+  width: 100%;
+  height: 100%;
   position: absolute;
-  width: 460px;
-  height: 580px;
-  top: 0;
   left: 0;
+  top: 0;
 }
 #map-draw-layer circle {
  cursor: pointer;
