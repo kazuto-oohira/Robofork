035b7171a2c9a02ef038a964bf024c1ae84463ac
Yuya Yamada <y@autotelic.jp>
2018-03-08 18:42:40 +0900

    マップのモード選択のUI追加

--- robofork_frontend/src/MapViewer.vue
+++ robofork_frontend/src/MapViewer.vue
@@ -3,6 +3,10 @@
     <div class="row">
       <div class="map-container"
         :style="{ width: `${width}px`, height: `${height}px` }"
+        :class="{
+          'routing': enableRouting,
+          'point-edit': enablePointEdit,
+        }"
         @click.self="mark($event.offsetX, $event.offsetY)"
       >
         <img v-if="imageUrl" class="map-image" :src="imageUrl">
@@ -31,6 +35,7 @@
               transform: `translate(${mappedX(node.x)}px, ${mappedY(node.y)}px)`
             }"
             :title="node.id"
+            @click.self="select(node)"
           >
             <img
               src="/static/robofork_app/img/robofork.svg"
@@ -66,16 +71,35 @@
       </div>
     </div>
     <div class="row">
-      <div class="checkbox">
-        <label>
-          <input type="checkbox" v-model="checkSubNodes"> サブノードも含めて表示する
-        </label>
+      <div class="col-sm-6">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" v-model="checkSubNodes"> サブノードも含めて表示する
+          </label>
+        </div>
+      </div>
+      <div class="col-sm-6 btn-group">
+        <button
+          class="btn btn-default"
+          :disabled="enableRouting"
+          :class="{ 'btn-primary': enableRouting }"
+          @click="selectRouting"
+        >ルート追加モード</button>
+        <button
+          class="btn btn-default"
+          :disabled="enablePointEdit"
+          :class="{ 'btn-primary': enablePointEdit }"
+          @click="selectPointEdit"
+        >ポイント編集モード</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+const MODE_ROUTING = 0;
+const MODE_POINT_EDIT = 1;
+
 export default {
   name: 'map-viewer',
 
@@ -97,6 +121,7 @@ export default {
   data() {
     return {
       checkSubNodes: true,
+      modeIndex: 0,
     }
   },
 
@@ -107,6 +132,14 @@ export default {
 
     enableRobofork() {
       return !!this.animate;
+    },
+
+    enableRouting() {
+      return this.modeIndex === MODE_ROUTING;
+    },
+
+    enablePointEdit() {
+      return this.modeIndex === MODE_POINT_EDIT;
     },
 
     unitX() {
@@ -189,10 +222,20 @@ export default {
 
   methods: {
     mark(x, y) {
+      // ルート追加モードでなければマップを選択しても何もしない
+      if (this.modeIndex !== MODE_ROUTING) {
+        return;
+      }
+
       this.$emit('addMark', {
         x: this.unmappedX(x),
         y: this.unmappedY(y),
       });
+    },
+
+    select(node) {
+      // 一旦情報を表示するだけ
+      alert(`id: ${node.id}, x: ${node.x}, y: ${node.y}, dir: ${node.dir}`);
     },
 
     mappedX(x) {
@@ -226,6 +269,14 @@ export default {
 
       return this.mainNodes[this.mainNodes.length - 1].id === id;
     },
+
+    selectRouting() {
+      this.modeIndex = MODE_ROUTING;
+    },
+
+    selectPointEdit() {
+      this.modeIndex = MODE_POINT_EDIT;
+    },
   },
 }
 </script>
@@ -235,7 +286,7 @@ export default {
   position: relative;
 }
 
-.map-container:after {
+.map-container.routing:after {
   width: 100%;
   height: 100%;
   content: '';
@@ -271,17 +322,24 @@ export default {
   background: blue;
   border-radius: 8px;
   opacity: 1;
+  cursor: pointer;
 }
 
 .map-draw-layer .mainnode.current {
+}
+
+.map-draw-layer .mainnode.current img {
   width: 30px;
   height: 30px;
+  position: absolute;
+  left: 8px;
+  top: 8px;
   margin: -15px 0 0 -15px;
-  background: transparent;
-  border-radius: 15px;
-}
-.map-draw-layer .mainnode.current img {
   transform-origin: 50% 50%;
+}
+
+.map-container.point-edit .map-draw-layer .mainnode.current img {
+  display: none;
 }
 
 .map-draw-layer .subnode {
