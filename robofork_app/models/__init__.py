637c51c9abe9f214625aafa9765e52e60a5a8b82
Shingo Mori <shingo-mori@azapa.co.jp>
2018-03-08 15:16:29 +0900

    奥田くんの残した不要ファイル削除

--- robofork_app/models/__init__.py
+++ robofork_app/models/__init__.py
@@ -1,2 +1,1 @@
 from .vehicle import Vehicle
-from .map import Map
--- robofork_app/models/map.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Map(models.Model):
-    # マップ画像ファイルをBase64にしてDBへ突っ込もうかと思っていた。が面倒になったのでひとまず止める。
-    pass
