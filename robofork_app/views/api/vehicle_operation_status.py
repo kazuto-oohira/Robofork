9a98a0f3922cef5b08b04bd673a8c171ea4ced21
Shingo Mori <shingo-mori@azapa.co.jp>
2018-03-14 08:42:13 +0900

    WebSocketのURLルーティング追加

--- robofork_app/views/api/vehicle_operation_status.py
+++ robofork_app/views/api/vehicle_operation_status.py
@@ -43,37 +43,3 @@ def load(request):
             ]
         }
     )
-
-
-@csrf_exempt
-def save(request, vehicle_operation_plan_id=1):
-    print(json.dumps(json.loads(request.body), indent=4))
-
-    try:
-        vehicle_operation_plan = VehicleOperationPlan.objects.get(pk=vehicle_operation_plan_id)
-    except VehicleOperationPlan.DoesNotExist:
-        vehicle_operation_plan = VehicleOperationPlan()
-
-    vehicle_operation_plan.id = vehicle_operation_plan_id
-    vehicle_operation_plan.route_operation_json = json.dumps(json.loads(request.body))
-    vehicle_operation_plan.save()
-
-    """
-    vehicle_form = VehicleForm(request.POST, instance=vehicle_operation_plan)
-    if vehicle_form.is_valid():
-        vehicle_form.save()
-        return redirect('vehicle_index')
-    else:
-        return render(request, 'robofork_app/vehicle_view/detail.html', {'form': vehicle_form})
-    """
-
-    return JsonResponse({'result': True})
-
-
-@csrf_exempt
-def load(request, vehicle_operation_plan_id=1):
-    vehicle_operation_plan = get_object_or_404(VehicleOperationPlan, pk=vehicle_operation_plan_id)
-    print(vehicle_operation_plan.route_operation_json)
-
-    data = json.loads(vehicle_operation_plan.route_operation_json)
-    return JsonResponse(data)
--- robofork_app/web_socket_urls.py
+++ robofork_app/web_socket_urls.py
@@ -6,4 +6,8 @@ channel_routing = [
     route("websocket.connect", mqtt_test_view.ws_add, path="/mqtt_test_ws"),
     route("websocket.receive", mqtt_test_view.ws_message, path="/mqtt_test_ws"),
     route("websocket.disconnect", mqtt_test_view.ws_disconnect, path="/mqtt_test_ws"),
+
+    route("websocket.connect", mqtt_test_view.ws_add, path="/vehicle_operation_status"),
+    route("websocket.receive", mqtt_test_view.ws_message, path="/vehicle_operation_status"),
+    route("websocket.disconnect", mqtt_test_view.ws_disconnect, path="/vehicle_operation_status"),
 ]
