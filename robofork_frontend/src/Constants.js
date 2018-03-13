23100f5558351048982ef0b48c56ffe171bdfe12
Yuya Yamada <y@autotelic.jp>
2018-03-13 16:46:58 +0900

    COMMANDS_ENDPOINT はいらなくなったので削除

--- robofork_frontend/src/Constants.js
+++ robofork_frontend/src/Constants.js
@@ -1,6 +1,5 @@
 // API など
 export const CONFIG_ENDPOINT = '/static/robofork_app/api/operation_control.json';
-export const COMMANDS_ENDPOINT = '/static/robofork_app/api/commands.json';
 export const LOAD_ENDPOINT = (id) => `/api/operation_plan/${id}/load`;
 export const SAVE_ENDPOINT = (id) => `/api/operation_plan/${id}/save`;
 
