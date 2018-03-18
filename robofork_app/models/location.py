b80fdeabdf6398eb88baeedfdc6e786916449a0a
Shingo Mori <shingo-mori@azapa.co.jp>
2018-03-18 11:54:48 +0900

    初期データ投入

--- robofork_app/models/location.py
+++ robofork_app/models/location.py
@@ -4,7 +4,7 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=100)
     map_image = models.BinaryField(null=True)
-    vehicle_initial_positions_json = models.TextField(null=True)
+    map_information_json = models.TextField(null=True)
     plc_data_json = models.TextField(null=True)
     fire_doors_json = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- robofork_app/models/vehicle_operation_plan.py
+++ robofork_app/models/vehicle_operation_plan.py
@@ -5,11 +5,10 @@ from .vehicle import Vehicle
 
 class VehicleOperationPlan(models.Model):
     name = models.CharField(max_length=100)
+    explain = models.TextField(null=True)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, null=True)
     priority = models.IntegerField(default=0)
     route_operation_json = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
