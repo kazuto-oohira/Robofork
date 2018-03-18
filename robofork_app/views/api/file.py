4842dc86b306df8372b506836a4f34222ba47e1c
Shingo Mori <shingo-mori@azapa.co.jp>
2018-03-18 13:19:31 +0900

    デバッグ出力削除のみ

--- robofork_app/views/api/file.py
+++ robofork_app/views/api/file.py
@@ -8,6 +8,5 @@ from robofork_app.models.file import File
 @csrf_exempt
 def file(request, file_id=-1):
     file_model = get_object_or_404(File, pk=file_id)
-    print(file_model.type)
     return HttpResponse(content=base64.b64decode(file_model.data), content_type=file_model.type)
 
