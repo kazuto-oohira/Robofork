036c9594674ce897cee877a37b473a7b29517ac0
Shingo Mori <shingo-mori@azapa.co.jp>
2018-03-19 16:42:30 +0900

    コメント変更のみ

--- robofork_app/views/api/location.py
+++ robofork_app/views/api/location.py
@@ -11,8 +11,10 @@ def map_config(request, location_id=-1):
     location_model = get_object_or_404(Location, pk=location_id)
     map_info = json.loads(location_model.map_information_json)
 
-    # 編集
+    # マップ情報編集
     map_info["config"]["imageUrl"] = urls.reverse('api_file', args=(location_id,))
     del map_info["config"]["imageFileId"]
+
+    # TODO: 車両情報をマップConfigへ追加。それとVehicleにLocation関連がなかった...
 
     return JsonResponse(map_info)
