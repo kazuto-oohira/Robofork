d72acc8dbcfdcbdb5ddf6b9427a1403b35f946c0
Shingo Mori <shingo-mori@azapa.co.jp>
2018-03-21 18:16:50 +0900

    SavePoint

---
+++ robofork_app/services/route_operation_status.py
@@ -0,0 +1,8 @@
+import time, json
+from .can_const import *
+from robofork_app.libs import utility, mqtt
+from robofork_app.models.vehicle_operation_plan import VehicleOperationPlan
+
+
+class RouteOperationStatus:
+    pass
