41412480eb189a4827bbf4aa3f3c4f2f11ffd763
Shingo Mori <shingo-mori@azapa.co.jp>
2018-03-22 08:46:23 +0900

    admin車両一覧ができた

--- robofork_app/urls.py
+++ robofork_app/urls.py
@@ -12,10 +12,17 @@ urlpatterns = [
 
     # NKC管理画面
     path('admin/location/', TemplateView.as_view(template_name='robofork_app/admin_location/index.html'), name='admin_location_index'),
-    path('admin/vehicle/', TemplateView.as_view(template_name='robofork_app/admin_vehicle/index.html'), name='admin_vehicle_index'),
+    # path('admin/vehicle/', TemplateView.as_view(template_name='robofork_app/admin_vehicle/index.html'), name='admin_vehicle_index'),
     path('admin/vehicle_status/', TemplateView.as_view(template_name='robofork_app/admin_vehicle_status/index.html'), name='admin_vehicle_status_index'),
     path('admin/user/', TemplateView.as_view(template_name='robofork_app/admin_user/index.html'), name='admin_user_index'),
     path('admin/setting/vehicle_type/', TemplateView.as_view(template_name='robofork_app/admin_setting/vehicle_type/index.html'), name='admin_setting_vehicle_type_user_index'),
+
+    # 管理画面だけど...初期に作ったのから流用したので色々あってない
+    path('admin/vehicle', vehicle_view.index, name='admin_vehicle_index'),
+    path('admin/vehicle/new', vehicle_view.new, name='vehicle_new'),
+    path('admin/vehicle/save', vehicle_view.save, name='vehicle_save'),
+    path('admin/vehicle/save/<int:vehicle_id>', vehicle_view.save, name='vehicle_save'),
+    path('admin/vehicle/<int:vehicle_id>', vehicle_view.detail, name='vehicle_detail'),
 
     # 各配置場所 管理画面
     path('<int:location_id>/', home_view.index, name='home_index'),
