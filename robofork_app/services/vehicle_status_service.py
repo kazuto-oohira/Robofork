9335a4fa68e6e0af0cb80bbbd780bd64e4bdbe95
Shingo Mori <shingo-mori@azapa.co.jp>
2018-03-22 10:46:41 +0900

    緊急停止スイッチのインターロック信号修正

--- robofork_app/services/vehicle_status_service.py
+++ robofork_app/services/vehicle_status_service.py
@@ -134,7 +134,7 @@ class VehicleStatusService:
         vehicle_status.interlock_lrf_front = 1 if (int(data["data"][1], 16) & 0b11110000) else 0
         vehicle_status.interlock_lrf_rear = 1 if (int(data["data"][1], 16) & 0b00001111) else 0
         vehicle_status.interlock_body_around_tape = 1 if (int(data["data"][0], 16) & 0b10000000) else 0
-        vehicle_status.interlock_emergency_button = 1 if (int(data["data"][0], 16) & 0b11100000) else 0
+        vehicle_status.interlock_emergency_button = 1 if (int(data["data"][0], 16) & 0b00001110) else 0
 
 
     def __set_vehicle_lift_slant(self, vehicle_status, data):
